4b8a686ee0136988da590b92fdc608c59f1fa626
Ulukbekov Naken <ulukbekov_n@auca.kg>
2023-09-09 01:27:02 +0600

    some changes in the UI

--- app/src/main/java/com/example/pmordo/data/networks/Constants.kt
+++ app/src/main/java/com/example/pmordo/data/networks/Constants.kt
@@ -1,5 +1,5 @@
 package com.example.pmordo.data.networks
 
 object Constants {
-    const val BASE_URL = "http://16.171.197.36/api/v1/"
+    const val BASE_URL = "http://54.90.163.29/api/v1/"
 }
--- app/src/main/java/com/example/pmordo/presentation/firstscreen/register/SellerRegisterFragment.kt
+++ app/src/main/java/com/example/pmordo/presentation/firstscreen/register/SellerRegisterFragment.kt
@@ -1,14 +1,17 @@
 package com.example.pmordo.presentation.firstscreen.register
 
 import android.os.Bundle
+import android.text.method.PasswordTransformationMethod
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.pmordo.R
 import com.example.pmordo.databinding.SellerRegisterFragmentBinding
 
 class SellerRegisterFragment: Fragment() {
     private lateinit var binding:SellerRegisterFragmentBinding
+    private var isPasswordVisible = true
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -16,5 +19,26 @@ class SellerRegisterFragment: Fragment() {
     ): View? {
         binding = SellerRegisterFragmentBinding.inflate(inflater,container,false)
         return binding.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        val passwordEditText = binding.inputPassword
+        val passwordVisibilityToggle = binding.passwordVisibilityToggle
+
+        passwordVisibilityToggle.setOnClickListener {
+            isPasswordVisible = !isPasswordVisible
+
+            if (isPasswordVisible) {
+                passwordEditText.transformationMethod = null
+                passwordVisibilityToggle.setImageResource(R.drawable.show_pass)
+            } else {
+                passwordEditText.transformationMethod = PasswordTransformationMethod()
+                passwordVisibilityToggle.setImageResource(R.drawable.hide_pass)
+            }
+
+            passwordEditText.text?.let { it1 -> passwordEditText.setSelection(it1.length) }
+        }
     }
 }
