27fc0627fb5b4a49aaac6166997987ceefff8dce
Ulukbekov Naken <ulukbekov_n@auca.kg>
2023-09-13 14:47:28 +0600

    some changes in the UI

--- app/src/main/java/com/example/pmordo/data/networks/Constants.kt
+++
@@ -1,5 +0,0 @@
-package com.example.pmordo.data.networks
-
-object Constants {
-    const val BASE_URL = "http://54.90.163.29/api/v1/"
-}
--- app/src/main/java/com/example/pmordo/domain/models/LoginRequest.kt
+++
@@ -1,6 +0,0 @@
-package com.example.pmordo.domain.models
-
-data class LoginRequest(
-    val email: String,
-    val password: String
-)
